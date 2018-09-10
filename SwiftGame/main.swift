85f3ea432c49d926467f890c2ce4a07719f4d2b0
Cindy Perat <cindy.perat@gmail.com>
2018-09-10 09:11:02 +0200

    First commit

---
+++ SwiftGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftGame
+//
+//  Created by Cindy Perat on 10/09/2018.
+//  Copyright © 2018 Cindy Perat. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
